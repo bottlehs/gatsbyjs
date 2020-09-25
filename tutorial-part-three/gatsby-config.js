fac3a5bb2a0e7e2cae0d0ce15a846c536fb283a6
bottlehs <qudgns9@gmail.com>
2020-09-25 18:44:42 +0900

    gatsby-tutorial-part-tree

---
+++ tutorial-part-three/gatsby-config.js
@@ -0,0 +1,17 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: "gatsby-plugin-typography",
+      options: {
+        pathToConfigModule: "src/utils/typography",
+      },
+    },
+  ],
+}
